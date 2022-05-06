5988be79486a297ba07f0a41747f123557294b83
kunnChen <71739331+kunnChen@users.noreply.github.com>
2022-05-05 17:06:08 -0800

    Connectivity and dynamic data

---
+++ ZEKJUNLP_2nd version/js/lib.js
@@ -0,0 +1,12 @@
+function floatMul(a, b) {
+	var c = 0,
+		d = a.toString(),
+		e = b.toString();
+	try {
+		c += d.split(".")[1].length;
+	} catch (f) {}
+	try {
+		c += e.split(".")[1].length;
+	} catch (f) {}
+	return Number(d.replace(".", "")) * Number(e.replace(".", "")) / Math.pow(10, c);
+}
